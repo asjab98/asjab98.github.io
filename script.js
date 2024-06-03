5d18e315a6d0992f42ebc091b6ede7382dd8b571
asjab98 <asja.bbk@gmail.com>
2024-06-03 11:49:05 +0200

    wip2

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Ciao")
+/* dateElement = document.querySelector("#date");
+
+today = new Date();
+
+dateElement.innerText = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+
+let d = new Date();
+document.getElementById("date").innerHTML = d; */
